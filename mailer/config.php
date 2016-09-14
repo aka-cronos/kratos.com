d49d29186ded36e0e347f58c59a884191dafbecd
Jorge Escamilla <akacronos@gmail.com>
2016-09-13 22:54:26 -0500

    Restore mailer folder

---
+++ mailer/config.php
@@ -0,0 +1,12 @@
+<?php
+
+  // Cuenta de correo de la cual mandar el correo
+  $mail_username = 'sales@kratosbp.com';
+  $mail_password = 'Kratos@2016';
+
+  // Configuracion del servidor de correo
+  $mail_host = 'smtp.gmail.com';
+  $mail_port = 465;
+  $mail_security = 'tls';
+
+?>
